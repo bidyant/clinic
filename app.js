a18e5e7049d78ac3e07967e83f644483c9e8c486
Bidya Bedant Joshi <65537036+bidyant@users.noreply.github.com>
2024-06-06 13:20:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -121,11 +121,14 @@ function displayItems(itemsToDisplay) {
         refTd.textContent = item.ref;
 
         if(item.ref=="HEAD"){
+            nameTd.id="head";
+            if(nameTd.id!="head"){
             nameTd.style.width='100%';
             nameTd.style.textAlign='center';
             nameTd.style.fontSize='18px';
             nameTd.style.fontWeight='bold';
             nameTd.style.textDecoration='underline';
+            }
             tr.appendChild(selectTd);
             tr.appendChild(nameTd);
         }else{
